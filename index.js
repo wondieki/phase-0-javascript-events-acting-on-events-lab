5b8e2b0432bb33415802fc009aded163333b2993
wondieki <wkerubo28@gmail.com>
2024-11-04 10:16:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+// I changed the value to 5 to increase the speed of the dodger.
+const dodger=document.getElementById("dodger")
+dodger.style.backgroundColor="red"
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    } else if (event.key === "ArrowUp") {
+      moveDodgerUp();
+    } else if (event.key === "ArrowDown") {
+      moveDodgerDown();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  if (left>0){dodger.style.left = `${left - 5}px`;}
+    
+  }
+  //this is based on the dimensions of the box
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 5}px`;
+    }
+  }
+  
+  function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+    if (bottom < 380) {
+      dodger.style.bottom = `${bottom + 5}px`;
+    }
+  }
+  
+  function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+    if (bottom > 0) {
+      dodger.style.bottom = `${bottom - 5}px`;
+    }
+  }
